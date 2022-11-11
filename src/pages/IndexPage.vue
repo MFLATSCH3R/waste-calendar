5ddb1f810a7c03ece91b35ea9b1a30c8afd7697c
MFLATSCH3R <43829482+MFLATSCH3R@users.noreply.github.com>
2022-11-11 16:27:40 +0100

    initial commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex flex-center">
+    <WeekDateType />
+  </q-page>
+</template>
+
+<script>
+import WeekDateType from 'components/CalendarWeek.vue'
+export default {
+  name: 'IndexPage',
+  components: {
+    WeekDateType
+  }
+}
+</script>
